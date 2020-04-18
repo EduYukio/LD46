5a3fa99f9116e2d177c37ca2a05dc66313a53d1f
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-04-18 16:06:30 -0300

    Add basic walnut movement

---
+++ Assets/Scripts/Walnut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walnut : MonoBehaviour {
+    public float fallMultiplier = 3f;
+    public Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        //SmoothFall();
+    }
+
+    public void SmoothFall() {
+        rb.gravityScale = 3;
+
+        if (rb.velocity.y < 0) {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+    }
+}
