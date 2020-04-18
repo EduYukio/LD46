a29b9cdfda306fba25b9d19b70927eb33853dc6e
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-04-18 04:52:41 -0300

    Now player can walk

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float walkSpeed = 3f;
+
+    void Start() {
+
+    }
+    void FixedUpdate() {
+        ProcessWalkInput();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void ProcessWalkInput() {
+        float x = Input.GetAxisRaw("Horizontal");
+
+        //if (x == 0 && y == 0) {
+        //    playerAnimator.SetBool("IsWalking", false);
+        //}
+        //else {
+        //    playerAnimator.SetBool("IsWalking", true);
+        //}
+
+        transform.Translate(x * Time.fixedDeltaTime * walkSpeed, 0, 0);
+    }
+}
